ebf79033fb729802f72a6c255d4f35b3fe19d384
Katelyn Rodrigue <kr@bit.ly>
2018-10-24 13:57:43 -0400

    initial commit

---
+++ SeleniumDemo.test.js
@@ -0,0 +1,61 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var webdriver = __importStar(require("selenium-webdriver"));
+var chai_1 = require("chai");
+var fs = __importStar(require("fs"));
+require("mocha");
+chai_1.should();
+// skipping this test run until configuring headless chrome for travis CI
+describe('Selenium Demo Test Suite', function () {
+    var driver;
+    // time out for test execution
+    this.timeout(60000);
+    before(function () {
+        // initializing chrome driver
+        driver = new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .build();
+        // maximizing chrome browser
+        driver.manage().window().maximize();
+    });
+    afterEach(function () {
+        var testCaseName = this.currentTest.title;
+        var testCaseStatus = this.currentTest.state;
+        if (testCaseStatus === 'failed') {
+            console.log("Test: " + testCaseName + ", Status: Failed!");
+            // capturing screenshot if test fails
+            driver.takeScreenshot().then(function (data) {
+                var screenshotPath = "./TestResults/Screenshots/" + testCaseName + ".png";
+                console.log("Saving Screenshot as: " + screenshotPath);
+                fs.writeFileSync(screenshotPath, data, 'base64');
+            });
+        }
+        else if (testCaseStatus === 'passed') {
+            console.log("Test: " + testCaseName + ", Status: Passed!");
+        }
+        else {
+            console.log("Test: " + testCaseName + ", Status: Unknown!");
+        }
+    });
+    after(function () {
+        driver.quit();
+    });
+    it('C2775 should load bitly.com logged out home page', function () {
+        var url = "http://bitly.com";
+        var domain = 'bitly.com';
+        return driver.get(url).then(function () {
+            console.log("Page \"" + url + "\" opened");
+        }).then(function () {
+            return driver.getCurrentUrl().then(function (currentUrl) {
+                currentUrl.should.include(domain, "Expected url: " + domain + ", Actual url: " + currentUrl);
+            });
+        });
+    });
+});
---
+++ SeleniumDemo.test.ts
@@ -0,0 +1,56 @@
+import * as webdriver from 'selenium-webdriver';
+import { should } from 'chai';
+import * as fs from 'fs';
+import 'mocha';
+should();
+
+describe('Selenium Demo Test Suite', function () {
+    let driver;
+    // time out for test execution
+    this.timeout(60000);
+    
+    before(function () {
+        // initializing chrome driver
+        driver = new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .build();
+        // maximizing chrome browser
+        driver.manage().window().maximize();
+    });
+    
+    afterEach(function () {
+        let testCaseName: string = this.currentTest.title;
+        let testCaseStatus: string | undefined = this.currentTest.state;
+        if (testCaseStatus === 'failed') {
+            console.log(`Test: ${testCaseName}, Status: Failed!`);
+            // capturing screenshot if test fails
+            driver.takeScreenshot().then((data) => {
+                let screenshotPath = `./TestResults/Screenshots/${testCaseName}.png`;
+                console.log(`Saving Screenshot as: ${screenshotPath}`);
+                fs.writeFileSync(screenshotPath, data, 'base64');
+            });
+        } else if (testCaseStatus === 'passed') {
+            console.log(`Test: ${testCaseName}, Status: Passed!`);
+        } else {
+            console.log(`Test: ${testCaseName}, Status: Unknown!`);
+        }
+    });
+
+    after(function () {
+        driver.quit();
+    });
+
+    it('C2775 should load bitly.com logged out home page', function () {
+        const url = "http://bitly.com";
+        const domain = 'bitly.com';
+        return driver.get(url).then(() => {
+            console.log(`Page "${url}" opened`);
+        }).then(() => {
+            return driver.getCurrentUrl().then((currentUrl) => {
+                currentUrl.should.include(
+                    domain,
+                    `Expected url: ${domain}, Actual url: ${currentUrl}`);
+            });
+        });
+    });
+});
